f3169a8bf609772e2ce8dd2bbe2977a3534fd7a3
princelizard <thetuss221@gmail.com>
2026-04-04 16:54:24 -0400

    added stuff

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
+use std::fs::{self, File};
 slint::include_modules!();
-fn main() -> Result<(), slint::platform::PlatformError> {
+
+#[tokio::main]
+async fn main() -> Result<(), slint::platform::PlatformError> {
+    create_file();
     let ui = MainWindow::new().unwrap();
 
     ui.run()
+}
+
+fn create_file(){
+    if !fs::exists("feeds.json").unwrap(){
+        File::create("feeds.json").expect("Failed to create feeds.json");
+    }
+}
+
+async fn add_feed(feed_url: String){
+    let body = reqwest::get(feed_url).await.unwrap().text().await.unwrap();
 }
