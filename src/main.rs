26c4a07f2e6a6f88ce3ef6a2719bcd452b7a6ad1
princelizard <thetuss221@gmail.com>
2026-04-05 14:10:01 -0400

    added ability to add feed to feed.json

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
 use std::fs::{self, File};
+
+use rss::Channel;
 slint::include_modules!();
+
+#[derive(serde::Serialize)]
+struct Feed {
+    title: String,
+    link: String,
+    description: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), slint::platform::PlatformError> {
     create_file();
     let ui = MainWindow::new().unwrap();
 
+    ui.on_submit_feed(move |feed_url| {
+        let feed_url = feed_url.to_string();
+        tokio::spawn(async move {
+            add_feed(feed_url).await;
+        });
+    });
     ui.run()
 }
 
@@ -16,5 +31,13 @@ fn create_file(){
 }
 
 async fn add_feed(feed_url: String){
-    let body = reqwest::get(feed_url).await.unwrap().text().await.unwrap();
+    let xml = reqwest::get(feed_url).await.unwrap().text().await.unwrap();
+    let channel = Channel::read_from(xml.as_bytes()).unwrap();
+    let feed = Feed {
+        title: channel.title().to_string(),
+        link: channel.link().to_string(),
+        description: channel.description().to_string(),
+    };
+    let json = serde_json::to_string(&feed).unwrap();
+    fs::write("feeds.json", json).expect("Failed to write to feeds.json");
 }
