aab973d320c3aaf8cc1587023f74914d185c00e9
princelizard <thetuss221@gmail.com>
2026-04-10 22:36:27 -0400

    refactor:  arc mutex'd the hashmap for later async shenanigans

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::io::Write;
 use std::collections::HashMap;
 use rss::Channel;
 use slint::{VecModel, ModelRc};
+use tokio::sync::Mutex;
+use std::sync::Arc;
 
 slint::include_modules!();
 
@@ -16,16 +18,16 @@ struct Feed {
 #[tokio::main]
 async fn main() -> Result<(), slint::platform::PlatformError> {
     let ui = MainWindow::new().unwrap();
-    let mut channels_map = generate_hashmap();
-
-    populate_channels(&channels_map, &ui).await;
+    let channels_map = Arc::new(Mutex::new(generate_hashmap()));
+    
+    populate_channels(channels_map.clone(), &ui).await;
 
     ui.on_submit_feed(move |feed_url| {
         let feed_url = feed_url.to_string();
         if feed_url.ends_with("xml") {
             tokio::spawn(async move {
-                if add_feed(feed_url).await.is_ok() {
-                    //code goes here
+                if let Ok(feed) = add_feed(feed_url).await {
+                    //update channels 
                 }
             });
         }
@@ -34,7 +36,7 @@ async fn main() -> Result<(), slint::platform::PlatformError> {
 }
 
 //NOTE: feeds.json is actually a jsonl file. This is improper, but it won't parse otherwise, and I can't be assed to fix it. 
-async fn add_feed(feed_url: String) -> Result<(), reqwest::Error> {
+async fn add_feed(feed_url: String) -> Result<(Feed), reqwest::Error> {
     let xml = reqwest::get(feed_url).await?.text().await?;
     let channel = Channel::read_from(xml.as_bytes()).unwrap();
     let feed = Feed {
@@ -47,7 +49,7 @@ async fn add_feed(feed_url: String) -> Result<(), reqwest::Error> {
     let mut file = File::options().append(true).create(true).open("feeds.json").unwrap();
     writeln!(file, "{}", json).unwrap();
     // should either regenerate hashmap or update it. do this by returning a true or hashmap reference or something. let the closure decide.
-    Ok(())
+    Ok(feed)
 }
 
 fn generate_hashmap() -> HashMap<String, String> {
@@ -61,11 +63,15 @@ fn generate_hashmap() -> HashMap<String, String> {
     hashmap
 }
 
-async fn populate_channels(map: &HashMap<String, String>, ui: &MainWindow) {
-    let channels: Vec<ChannelData> = map.iter().map(|(title, link)| ChannelData {
+async fn populate_channels(map: Arc<Mutex<HashMap<String, String>>>, ui: &MainWindow) {
+    let channels: Vec<ChannelData> = map.lock().await.iter().map(|(title, link)| ChannelData {
         channel_title: title.into(),
         channel_url: link.into(),
     }).collect();
     let model = ModelRc::new(VecModel::from(channels));
     ui.set_channels(model);
+}
+
+async fn update_hashmap(map: Arc<Mutex<HashMap<String, String>>>, title: String, link: String) {
+    map.lock().await.insert(title, link);
 }
