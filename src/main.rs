b91062de6f93ddf1e2ae06a30be2857d3567e58b
princelizard <thetuss221@gmail.com>
2026-04-11 19:36:53 -0400

    removed dependency tokio and replaced reqwest with ureq

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,7 @@ use std::io::Write;
 use std::collections::HashMap;
 use rss::Channel;
 use slint::{VecModel, ModelRc};
-use tokio::sync::Mutex;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 
 slint::include_modules!();
 
@@ -15,20 +14,19 @@ struct Feed {
     description: String,
 }
 
-#[tokio::main]
-async fn main() -> Result<(), slint::platform::PlatformError> {
+fn main() -> Result<(), slint::platform::PlatformError> {
     let ui = MainWindow::new().unwrap();
     let channels_map = Arc::new(Mutex::new(generate_hashmap()));
-    populate_channels(channels_map.clone(), &ui).await; 
+    populate_channels(channels_map.clone(), &ui); 
     
     ui.on_submit_feed(move |feed_url| { // this needs to generate a ui borrow
         let feed_url = feed_url.to_string();
         let map_clone = Arc::clone(&channels_map);
 
         if feed_url.ends_with("xml") { //TODO: show the user an angry rectangle on invalid link
-            tokio::spawn(async move {
-                if let Ok(feed) = add_feed(feed_url).await {
-                    let mut map = map_clone.lock().await;
+            std::thread::spawn(move || {
+                if let Ok(feed) = add_feed(feed_url) {
+                    let mut map = map_clone.lock().unwrap();
                     map.insert(feed.title, feed.link);
                 }
             });
@@ -39,8 +37,8 @@ async fn main() -> Result<(), slint::platform::PlatformError> {
         let ui_weak = ui.as_weak();
         move |channel_info| {
             let ui_weak = ui_weak.clone();
-            tokio::spawn(async move {
-                populate_episodes(channel_info, ui_weak).await;
+            std::thread::spawn( move || {
+                populate_episodes(channel_info, ui_weak);
             }); 
         }
     });
@@ -49,9 +47,9 @@ async fn main() -> Result<(), slint::platform::PlatformError> {
 }
 
 //NOTE: feeds.json is actually a jsonl file. This is improper, but it won't parse otherwise. #whocare 
-async fn add_feed(feed_url: String) -> Result<Feed, reqwest::Error> {
+fn add_feed(feed_url: String) -> Result<Feed, ureq::Error> {
     let feed_url_copy = feed_url.clone();
-    let xml = reqwest::get(feed_url).await?.text().await?;
+    let xml = ureq::get(feed_url).call()?.body_mut().read_to_string()?;
     let contents = Channel::read_from(xml.as_bytes()).unwrap();
     
     let feed = Feed {
@@ -78,8 +76,8 @@ fn generate_hashmap() -> HashMap<String, String> {
     hashmap
 }
 
-async fn populate_channels(map: Arc<Mutex<HashMap<String, String>>>, ui: &MainWindow) {
-    let channels: Vec<ChannelData> = map.lock().await.iter().map(|(title, link)| ChannelData {
+fn populate_channels(map: Arc<Mutex<HashMap<String, String>>>, ui: &MainWindow) {
+    let channels: Vec<ChannelData> = map.lock().unwrap().iter().map(|(title, link)| ChannelData {
         channel_title: title.into(),
         channel_url: link.into(),
     }).collect();
@@ -88,8 +86,8 @@ async fn populate_channels(map: Arc<Mutex<HashMap<String, String>>>, ui: &MainWi
     ui.set_channels(channels);
 }
 
-async fn populate_episodes(channel_info: ChannelData, ui: slint::Weak<MainWindow>) {
-    let xml = reqwest::get(channel_info.channel_url.to_string()).await.unwrap().text().await.unwrap();
+fn populate_episodes(channel_info: ChannelData, ui: slint::Weak<MainWindow>) {
+    let xml = ureq::get(channel_info.channel_url.to_string()).call().unwrap().body_mut().read_to_string().unwrap();
     let contents = Channel::read_from(xml.as_bytes()).unwrap();
     let episodes: Vec<EpisodeData> = contents.items().iter().map(|item| EpisodeData {
         audio_url: item.enclosure().map(|e| e.url().to_string()).unwrap_or_default().into(),
